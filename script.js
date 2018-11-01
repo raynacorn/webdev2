0d321382f0d2114cb354afbe8adcca0a9b0d3d99
Rayna C Corning <corni066@ad.umn.edu>
2018-11-01 11:32:13 -0500

    added csv

--- script.js
+++ script.js
@@ -27,5 +27,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // FIXME: Load CSV data into leaflet markers
 
+omnivore.csv('airports.csv').addTo(map);
+
+
 // FIXME: Add styling to markers
 // Use airport.png
